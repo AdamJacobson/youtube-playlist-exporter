2851ced529f69f34732b10581ccecd2b28dd172a
adam.jacobson <amartenj@gmail.com>
2025-02-25 09:52:42 -0800

    Version 0.14
    - Playlist metadata is now collected and exported in the JSON export file. Metadata includes:
      - Playlist title and url
      - Author name and url
      - Count of videos (total, hidden, unavailable)
      - Visibility, type, description
    - Code changes:
      - Page data is now stored in one location alongside metadata for simplicity so it doesn't have to be passed everywhere
      - Added test mode which prevents file downloading and just console logs the data that would be downloaded.
      - Reorganized method order and did code cleanup.
      - No longer need unavailable video indicies; just the count.

--- README.md
+++ README.md
@@ -1,12 +1,41 @@
 # youtube-playlist-exporter
 
+Version: 0.14
+
 This is a JavaScriptlet which allows you to download any youtube playlist as a text file.
 
 It first existed as script to be run inside of TamperMonkey which was then broken when a recent Chrome update changed how third party scripts could run (or not run).
 
 Currently, it must be copy pasted into your browser console in order to run.
 
+# Features
+
+- [ ] Export playlists as a text file
+- [ ] Export playlist data as a JSON object file
+
+## Planned features
+
+- [ ] UI with options
+- [ ] Able to be used with TamperMonkey
+- [ ] Chrome Plugin
+
 # Version History
+
+### 0.15 (Planned)
+- Make script work for playlists the user doesn't own
+- Detect when run for the first time when there are unavailable videos and prompt for reload and re-run
+
+### 0.14
+- Playlist metadata is now collected and exported in the JSON export file. Metadata includes:
+  - Playlist title and url
+  - Author name and url
+  - Count of videos (total, hidden, unavailable)
+  - Visibility, type, description
+- Code changes:
+  - Page data is now stored in one location alongside metadata for simplicity so it doesn't have to be passed everywhere
+  - Added test mode which prevents file downloading and just console logs the data that would be downloaded.
+  - Reorganized method order and did code cleanup.
+  - No longer need unavailable video indicies; just the count.
 
 ### 0.13
 - Data is now stored in JSON format to allow for easier formatting. Formatting happens on export.
--- export_youtube_playlist.js
+++ export_youtube_playlist.js
@@ -1,94 +1,61 @@
 window.exportPlaylistTitles = {
-  run: function () {
-    const videos = this.videosAsJSON();
+  lastRunPageData: null,
+  currentPageData: null,
+  interval: null,
+  mode: null,
 
-    let unavailableVideoIndicies = [];
-    const numHidden = this.numberOfHiddenVideos();
-    window.currentVideos = videos;
-    if (numHidden > 0) {
-      this.lastRunVideos = videos
+  run: function (mode) {
+    this.mode = mode;
+    this.currentPageData = this.getCurrentPageData();
 
-      console.log(`%c${numHidden} videos are hidden. Click the "Show unavailable videos" button to run this again.`, "color:red")
+    const numberOfHiddenVideos = this.currentPageData.metadata.numHiddenVideos;
+    if (numberOfHiddenVideos > 0) {
+      this.lastRunPageData = this.currentPageData;
+
+      console.log(`%c${numberOfHiddenVideos} videos are hidden. Click the "Show unavailable videos" button to run this again and detect which ones are unavailable.`, "color:red");
+      // alert(`${numberOfHiddenVideos} videos are hidden.\nAvailable videos have been recorded.\nClick the "Show unavailable videos" button to run this again.`);
       this.setupWaitForReload();
       return;
     } else {
-      if (this.lastRunVideos.length > 0) {
-        unavailableVideoIndicies = this.compareVideosWithPreviousRun(videos);
+      if (!!this.lastRunPageData) {
+        this.compareVideosWithPreviousRun();
       }
     }
 
-    const filename = this.filename(videos.length, unavailableVideoIndicies.length);
-
-    this.download(JSON.stringify(videos), filename, 'text/javascript');
-
-    this.exportFormattedVideoData(videos, unavailableVideoIndicies);
+    this.downloadRawData();
+    this.downloadFormattedData();
   },
 
-  exportFormattedVideoData: function (videos, unavailableVideoIndicies) {
-    const fileContents = [];
-    const header = this.documentHeader(videos.length, unavailableVideoIndicies.length);
-    const filename = this.filename(videos.length, unavailableVideoIndicies.length);
+  getCurrentPageData: function () {
+    const [videos, numUnavailableVideos] = this.getCurrentPageVideoData();
+    const metadata = this.getCurrentPageMetadata();
+    metadata.numUnavailableVideos = numUnavailableVideos;
 
-    fileContents.push(header);
-    fileContents.push(this.documentKey());
-
-    videos.forEach((video, index) => {
-      const available = !!video["available"];
-      const availability = available ? '' : 'UNAVAILABLE ';
-      let channelLink;
-
-      if (!!video["channelUrl"]) {
-        channelLink = `[${video["channelTitle"]}](${video["channelUrl"]})`;
-      } else {
-        channelLink = video["channelTitle"];
-      }
-
-      let line = `${index + 1}: ${availability}[${video["videoTitle"]}](${video["videoUrl"]}) | ${channelLink}`;
-      if (available) {
-        line = `- [] **${line}**`;
-      }
-
-      if (!available) {
-        fileContents.push("");
-      }
-
-      fileContents.push(line);
-    });
-
-    this.download(fileContents.join("\n"), filename, 'text/plain');
+    return {
+      metadata: metadata,
+      videos: videos,
+    };
   },
 
-  // Source: https://stackoverflow.com/questions/47942244/print-javascript-object-to-txt-file
-  download: function (data, filename, type) {
-    const file = new Blob([data], { type: type });
-    if (window.navigator.msSaveOrOpenBlob) // IE10+
-      window.navigator.msSaveOrOpenBlob(file, filename);
-    else { // Others
-      const a = document.createElement("a");
-      const url = URL.createObjectURL(file);
-      a.href = url;
-      a.download = filename;
-      document.body.appendChild(a);
-      a.click();
-      setTimeout(function () {
-        document.body.removeChild(a);
-        window.URL.revokeObjectURL(url);
-      }, 0);
-    }
-  },
-
-  videosAsJSON: function () {
+  getCurrentPageVideoData: function () {
     const videos = [];
     const videosAndSections = document.querySelectorAll("div#contents div#meta, div#title.ytd-item-section-header-renderer");
+    let numUnavailableVideos = 0;
 
     for (const [_, videoOrSection] of Object.entries(videosAndSections)) {
       if (videoOrSection.id === 'title') {
         break;
       }
-      videos.push(this.videoData(videoOrSection));
+
+      const data = this.videoData(videoOrSection);
+      if (!data.available) {
+        numUnavailableVideos++;
+      }
+
+      videos.push(data);
     }
 
-    return videos;
+    return [videos, numUnavailableVideos];
   },
 
   videoData: function (video) {
@@ -120,24 +87,107 @@ window.exportPlaylistTitles = {
     return data;
   },
 
-  interval: null,
+  getCurrentPageMetadata: function () {
+    const infoElement = document.querySelector("div.page-header-view-model-wiz__page-header-content");
+
+    const playlistTitle = infoElement.querySelector("h1").innerText;
+
+    const metadataElement = infoElement.querySelector("yt-content-metadata-view-model");
+
+    const author = metadataElement.querySelector("a");
+    const authorName = author.innerText.replace(/^by /, "");
+    const authorUrl = author.href;
+
+    const details = metadataElement.querySelectorAll("span.yt-core-attributed-string")
+    const type = details[1].innerText;
+    const visibility = details[2].innerText;
+    const numVideos = parseInt(details[3].innerText);
+    const numViews = parseInt(details[4].innerText);
+
+    const description = infoElement.querySelector("truncated-text-content span")?.innerText || null;
+
+    return {
+      playlistTitle: playlistTitle,
+      playlistUrl: document.location.href,
+      type: type,
+      visibility: visibility,
+      numVideos: numVideos,
+      numHiddenVideos: this.getNumberOfHiddenVideos(),
+      numViews: numViews,
+      authorName: authorName,
+      authorUrl: authorUrl,
+      description: description,
+    };
+  },
+
+  filename: function () {
+    const pageTitle = this.currentPageData.metadata.playlistTitle;
+    const numVideos = this.currentPageData.metadata.numVideos;
+    const numUnavailableVideos = this.currentPageData.metadata.numUnavailableVideos;
+
+    let header = `${pageTitle} as of ${this.timestamp()} (${numVideos} videos`
+
+    if (numUnavailableVideos > 0) {
+      header += `, ${numUnavailableVideos} unavailable`;
+    }
+
+    header += ')';
+
+    return header;
+  },
+
+  timestamp: function () {
+    const now = new Date;
+    const minutes = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
+    const seconds = now.getSeconds() < 10 ? `0${now.getSeconds()}` : now.getSeconds();
+    return `${now.getMonth() + 1}/${now.getDate()}/${now.getFullYear()} @ ${now.getHours()}:${minutes}:${seconds}`;
+  },
+
+  // Source: https://stackoverflow.com/questions/47942244/print-javascript-object-to-txt-file
+  download: function (data, filename, type) {
+    if (this.mode === 'test') {
+      console.log(this.filename(), type);
+      try {
+        const parsed = JSON.parse(data);
+        console.log(parsed);
+      } catch {
+        console.log(data);
+      }
+      return;
+    }
+
+    const file = new Blob([data], { type: type });
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+      window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+      const a = document.createElement("a");
+      const url = URL.createObjectURL(file);
+      a.href = url;
+      a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+      setTimeout(function () {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+      }, 0);
+    }
+  },
 
   setupWaitForReload: function () {
     this.interval = setInterval(() => {
       const alert = document.querySelector("div#alerts yt-formatted-string");
       if (alert.innerText === "Unavailable videos will be hidden during playback") {
         clearInterval(this.interval);
-        this.run();
+        this.run(this.mode);
       }
     }, 500);
   },
 
-  lastRunVideos: [],
-
   // Given the current list of videos, compare with the previous run and determine which ones are unavailable
-  compareVideosWithPreviousRun: function (currentVideos) {
-    const previousVideos = this.lastRunVideos;
-    const unavailableVideoIndicies = [];
+  compareVideosWithPreviousRun: function () {
+    const previousVideos = this.lastRunPageData.videos;
+    const currentVideos = this.currentPageData.videos;
+    let numUnavailableVideos = 0;
 
     const endIndex = Math.max(previousVideos.length, currentVideos.length);
     let index1 = 0;
@@ -147,21 +197,21 @@ window.exportPlaylistTitles = {
       const v1 = previousVideos[index1];
       const v2 = currentVideos[index2];
 
-      if (v1["videoUrl"] !== v2["videoUrl"]) {
-        v2["available"] = false;
-        unavailableVideoIndicies.push(index2);
+      if (v1.videoUrl !== v2.videoUrl) {
+        v2.available = false;
+        numUnavailableVideos++;
         index2++;
       } else {
-        v2["available"] = true;
+        v2.available = true;
         index1++;
         index2++;
       }
     }
 
-    return unavailableVideoIndicies;
+    this.currentPageData.metadata.numUnavailableVideos = numUnavailableVideos;
   },
 
-  numberOfHiddenVideos: function () {
+  getNumberOfHiddenVideos: function () {
     const alert = document.querySelector("div#alerts yt-formatted-string");
     if (alert !== null) {
       const match = alert.innerText.match(/(?<num>\d*) unavailable videos are hidden/);
@@ -172,9 +222,57 @@ window.exportPlaylistTitles = {
     return 0;
   },
 
-  documentHeader: function (numVideos, numUnavailableVideos) {
-    const pageTitle = document.title.replace(" - YouTube", "");
-    const pageUrl = document.location.href;
+  documentKey: function () {
+    return [
+      "- **Bolded entries have not yet been downloaded**",
+      "- A checkbox indicates it has been downloaded and imported to the library.",
+      "# Videos",
+    ].join("\n")
+  },
+
+  downloadRawData: function() {
+    this.download(JSON.stringify(this.currentPageData), this.filename(), 'text/javascript');
+  },
+
+  downloadFormattedData: function () {
+    const fileContents = [];
+    const header = this.documentHeader();
+
+    fileContents.push(header);
+    fileContents.push(this.documentKey());
+
+    const videos = this.currentPageData.videos;
+    videos.forEach((video, index) => {
+      const available = video.available;
+      const availability = available ? '' : 'UNAVAILABLE ';
+      let channelLink;
+
+      if (!!video.channelUrl) {
+        channelLink = `[${video.channelTitle}](${video.channelUrl})`;
+      } else {
+        channelLink = video.channelTitle;
+      }
+
+      let line = `${index + 1}: ${availability}[${video.videoTitle}](${video.videoUrl}) | ${channelLink}`;
+      if (available) {
+        line = `- [] **${line}**`;
+      }
+
+      if (!available) {
+        fileContents.push("");
+      }
+
+      fileContents.push(line);
+    });
+
+    this.download(fileContents.join("\n"), this.filename(), 'text/plain');
+  },
+
+  documentHeader: function () {
+    const pageTitle = this.currentPageData.metadata.playlistTitle;
+    const pageUrl = this.currentPageData.metadata.playlistUrl;
+    const numVideos = this.currentPageData.metadata.numVideos;
+    const numUnavailableVideos = this.currentPageData.metadata.numUnavailableVideos;
 
     let header = `[${pageTitle}](${pageUrl}) as of ${this.timestamp()} (${numVideos} videos`
 
@@ -186,35 +284,6 @@ window.exportPlaylistTitles = {
 
     return header;
   },
-
-  filename: function (numVideos, numUnavailableVideos) {
-    const pageTitle = document.title.replace(" - YouTube", "");
-
-    let header = `${pageTitle} as of ${this.timestamp()} (${numVideos} videos`
-
-    if (numUnavailableVideos > 0) {
-      header += `, ${numUnavailableVideos} unavailable)`;
-    } else {
-      header += ')';
-    }
-
-    return header;
-  },
-
-  documentKey: function () {
-    return [
-      "- **Bolded entries have not yet been downloaded**",
-      "- A checkbox indicates it has been downloaded and imported to the library.",
-      "# Videos",
-    ].join("\n")
-  },
-
-  timestamp: function () {
-    const now = new Date;
-    const minutes = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
-    const seconds = now.getSeconds() < 10 ? `0${now.getSeconds()}` : now.getSeconds();
-    return `${now.getMonth() + 1}/${now.getDate()}/${now.getFullYear()} @ ${now.getHours()}:${minutes}:${seconds}`;
-  },
 }
 
-window.exportPlaylistTitles.run();
+window.exportPlaylistTitles.run('test');
